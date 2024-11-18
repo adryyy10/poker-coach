57307f79d2750afc1189c8bf10c759ec53a1b9e3
adriafigueres <adria.figueres@m10c.com>
2024-11-18 11:20:03 +0000

    feat: user boolean filter

--- tests/Unit/BaseTestClass.php
+++ tests/Unit/BaseTestClass.php
@@ -14,23 +14,4 @@ class BaseTestClass extends TestCase
             'Accept' => 'application/ld+json',
         ])->get($uri);
     }
-
-    protected function createRegularUsers(): void
-    {
-        User::factory()->create([
-            'name' => 'John',
-        ]);
-    }
-
-    protected function createCoaches(): void
-    {
-        User::factory()->create([
-            'name' => 'adri',
-            'bio' => 'High stakes coach with over 5 years of experience in NL1000',
-            'is_coach' => true,
-            'specialty' => ['cash games', 'MTTs'],
-            'stakes' => ['mid', 'high'],
-            'price_per_hour' => 150.00,
-        ]);
-    }
 }
--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -2,17 +2,21 @@
 
 namespace Tests\Unit;
 
+use Database\Seeders\UserSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserTest extends BaseTestClass
 {
     use RefreshDatabase;
 
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->seed(UserSeeder::class);
+    }
+
     public function testGetCollection(): void
     {
-        $this->createCoaches();
-        $this->createRegularUsers();
-
         $response = $this->getRequest('/api/users');
         $response->assertOk();
         $response->assertJsonCount(2, 'member');
@@ -25,5 +29,23 @@ class UserTest extends BaseTestClass
         $this->assertEquals(['cash games', 'MTTs'], $firstUser?->specialty);
         $this->assertEquals(['mid', 'high'], $firstUser?->stakes);
         $this->assertEquals(150.00, $firstUser?->pricePerHour);
+    }
+
+    public function testFiltersCoaches(): void
+    {
+        $response = $this->getRequest('/api/users?is_coach=true');
+        $response->assertOk();
+        $response->assertJsonCount(1, 'member');
+        $this->assertEquals('adri', $response->json('member.0.name'));
+        $this->assertTrue($response->json('member.0.isCoach'));
+    }
+    
+    public function testFiltersNonCoaches(): void
+    {
+        $response = $this->getRequest('/api/users?is_coach=false');
+        $response->assertOk();
+        $response->assertJsonCount(1, 'member');
+        $this->assertEquals('John', $response->json('member.0.name'));
+        $this->assertFalse($response->json('member.0.isCoach'));
     }
 }
